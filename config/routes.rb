ba8e271030e0549923e3198113255eb2d237a58b
AMT98 <aswinmalla12@gmail.com>
2024-12-08 17:20:38 -0700

    Created controllers for signup/login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "session#new"
+
+  #user signup
+  get "signup", to: "users#new", as: :signup
+  post "signup", to: "users#create"
+
+  #Session login/logout
+  get "login", to: "session#new", as: :login
+  post "login", to: "session#create"
+  delete "logout", to: "session#destory", as: :logout
+
+  #Task routes
+  resources :tasks
 end
